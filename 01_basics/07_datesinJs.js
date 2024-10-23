adf665975779d9782c242f839c570b2bd05ff774
ayanali <132213787+ayanaliahmedali@users.noreply.github.com>
2024-10-23 20:09:42 +0000

    07_datesinJs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,16 @@
+// dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date("2023, 1, 23")
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Data.now()
+console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
