1aca126d7ad3c1bace920b4c6e7142a2ea3f476c
ayanali <132213787+ayanaliahmedali@users.noreply.github.com>
2024-10-24 07:36:24 +0000

    07_datasinJs.js all work update

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,16 +1,25 @@
 // dates
 
 let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
-let myCreatedDate = new Date("2023, 1, 23")
+let myCreatedDate = new Date("1-04-2023")
 // console.log(myCreatedDate.toDateString());
 
-let myTimeStamp = Data.now()
-console.log(myTimeStamp);
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date()
+// console.log(newDate);
 
+// `${newDate.getDay()} and the time` 
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
