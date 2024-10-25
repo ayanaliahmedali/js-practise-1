8e8991b6469256a8872fc02912ba0cda7fb7fc6c
ayanali <132213787+ayanaliahmedali@users.noreply.github.com>
2024-10-25 14:19:46 +0000

    json format

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -50,12 +50,30 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "ayan"
+}
 
+// course.courseInstructor
+
+const{price: rupee} = course
+
+// console.log(price);
+console.log(rupee);
+
+// {
+//     name: "ayan",
+//     coursename: "js in urdu",
+//     price: "free"
+// }
